fc5eea8be8e8dc8700d1f9660e05f0fe7e076a6f
tpatil2 <tpatil2@mail.csuchico.edu>
2017-03-29 15:40:20 -0700

    Arguments Parsing Linux cmd

---
+++ arg_parsing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "This is first argument: $0"
+echo "This is Second argument: $1"
+echo "This is Third argument: $2"
+echo "This is Fourth argument: $3"
+echo "These are All argument: $*"
+echo "Total num of arguments: $#"
